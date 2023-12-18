ad01f2340295f4d75d0e58bd88c2c749e0e2195c
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-18 23:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+let exicute=(event)=>
+	{
+		event.preventDefault();
+		alert(event.target.FirstName.value);
+		alert(event.target.LastName.value);
+		alert(event.target.LastName.value);
+		alert(event.target.PhoneNumber.value);
+		alert(event.target.EmailID.value)
 }
